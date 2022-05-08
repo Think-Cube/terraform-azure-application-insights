e1ec81a2a73b4b3cd2e8554544e621a7066595c3
spy86 <contact@mmichalski.com>
2022-05-08 14:47:30 +0200

    update

--- variables.tf
+++ variables.tf
@@ -33,28 +33,24 @@ variable "resource_group_name" {
 ###########################
 variable "application_insights_name" {
   description = "Specifies the name of the Application Insights component. Changing this forces a new resource to be created."
-  type = "string"
+  type = string
 }
 variable "application_insights_type" {
   description = "Specifies the type of Application Insights to create. Valid values are ios for iOS, java for Java web, MobileCenter for App Center, Node.JS for Node.js, other for General, phone for Windows Phone, store for Windows Store and web for ASP.NET. Please note these values are case sensitive; unmatched values are treated as ASP.NET by Azure. Changing this forces a new resource to be created."
-  type = "string"
+  type = string
   default = "web"
 }
 variable "log_analytics_workspace_name" {
   description = "Specifies the name of the Log Analytics Workspace. Workspace name should include 4-63 letters, digits or '-'. The '-' shouldn't be the first or the last symbol. Changing this forces a new resource to be created"
-  type = "string"
+  type = string
 }
 variable "log_analytics_workspace_sku" {
   description = "Specifies the SKU of the Log Analytics Workspace. Possible values are Free, PerNode, Premium, Standard, Standalone, Unlimited, CapacityReservation, and PerGB2018 (new SKU as of 2018-04-03). Defaults to PerGB2018."
-  type = "string"
+  type = string
   default = "PerGB2018"
 }
 variable "log_analytics_workspace_retention_in_days" {
   description = "The workspace data retention in days. Possible values are either 7 (Free Tier only) or range between 30 and 730."
-  type = "number"
+  type = number
   default = "30"
 }
-
-
-
-
